5fbc27d61b429a19e84ac332dbe11421d18f12ce
AKrutskih <52138488+AKrutskih@users.noreply.github.com>
2019-06-26 23:36:34 +0300

    Update pthreads.c

--- pthreads.c
+++ pthreads.c
@@ -7,14 +7,14 @@
 #define n 3
 
     double x[n];
-    double a[n][n] = {{1,2,3},{3,5,7},{1,3,4}};
-    double b[n] = {3,0,1};
+    double a[n][n] //= {{1,2,3},{3,5,7},{1,3,4}};
+    double b[n] //= {3,0,1};
 
 void pth_funk(void *arg)
 {
     int k = * (int *) arg;
-printf("%d", k);
-double del_x = a[k][k];
+//printf("%d", k);
+    double del_x = a[k][k];
     for (int i=k+1; i<n; i++)
     {
         double koef = a[i][k]/del_x;
@@ -26,7 +26,15 @@ double del_x = a[k][k];
 
 int main (int argc, char *argv[])
 {
-
+//Кривая генерация
+    for (int i=0; i<n; i++)
+    {
+        srand(time(NULL));
+        for (int j=0; j<n; j++)
+            a[i][j] = rand() % 100 + 1;
+        b[i] = rand() % 100 + 1;
+    }
+    
     //Печать
     for (int i=0; i<n; i++)
     {
