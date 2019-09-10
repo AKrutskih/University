1ac7eafe53f3b04b63549f3927012fad13078371
AKrutskih <52138488+AKrutskih@users.noreply.github.com>
2019-09-10 14:58:58 +0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -10,3 +10,16 @@ fig.colorbar (ax)
 fig.set_size_inches (8.5 , 8.5)
 #fig.savefig ( file_name , dpi =100)
 plt.close ( fig )
+
+
+fig = plt . figure ()
+59 ax = fig. add_subplot (111)
+60 pl = ax. imshow ( graph . get_matrix () , cmap = plt . get_cmap (" gist_earth ") )
+61 fig . colorbar (pl)
+62 fig . set_size_inches (8.5 , 8.5)
+63 ax. plot ([x for x, y in path ] , [y for x, y in path ] , linewidth =2.0 , c="
+,→ orange ")
+64 ax. plot ( path [0][0] , path [0][1] , "ro", c=" black ")
+65 ax. plot ( path [len( path ) - 1][0] , path [ len ( path ) - 1][1] , "ro", c=" red ")
+66 fig . savefig ( file_name , dpi =100)
+67 plt . close ( fig )
