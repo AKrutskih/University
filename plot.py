92956e31df4a78897dc6ac0c7fc230645fa2da6c
AKrutskih <52138488+AKrutskih@users.noreply.github.com>
2019-09-10 14:54:32 +0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,11 +4,9 @@ import numpy as np
 
 myMap = np.loadtxt('MAPS/Map25Iter0.txt')
 
-fig  = plt.figure()
-plt.colorbar(plt.imshow(myMap, origin="lower", cmap=cm.jet))
-plt.title('Simple imshow plot')
-'''for j in myMap:
-    for i in j:
-        plt.scatter(i[0][0], i[1][0], marker='D', linewidths=0.01, color='k')  # финиш
-        plt.scatter(i[0][-1], i[1][-1], marker='o', linewidths=0.01, color='k')  # старт
-        plt.plot(i[0], i[1], c='k', linewidth=1.2)'''
+fig = plt.figure ()
+ax = plt.imshow (myMap , cmap = plt . get_cmap (" gist_earth ") )
+fig.colorbar (ax)
+fig.set_size_inches (8.5 , 8.5)
+#fig.savefig ( file_name , dpi =100)
+plt.close ( fig )
